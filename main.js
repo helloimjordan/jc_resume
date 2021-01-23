b0e950c9be6540a4ecc96fc38e4b7761cea89c8c
himynamesjordan <jlcates42@gmail.com>
2021-01-22 16:03:58 -0800

    added new photo

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
     document.getElementById("nav_bar").style.padding = "30px 10px";
-    document.getElementById("navbar-rightlinks").style.fontSize = "25px";
+    document.getElementById("navbar_rightlinks").style.fontSize = "25px";
     document.getElementById("nav_bar").style.opacity=".6";
   } else {
     document.getElementById("nav_bar").style.padding = "80px 10px";
-    document.getElementById("navbar-rightlinks").style.fontSize = "30px";
+    document.getElementById("navbar_rightlinks").style.fontSize = "30px";
     document.getElementById("nav_bar").style.opacity=".9";
   }
 } 
