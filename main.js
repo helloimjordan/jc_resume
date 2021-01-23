dac09ae695f10bc94cfbb7550da5be9e55494ec0
himynamesjordan <jlcates42@gmail.com>
2021-01-22 16:14:41 -0800

    changed nav link position

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    document.getElementById("nav_bar").style.padding = "30px 10px";
+    document.getElementById("nav_bar").style.padding = "50px 10px";
     document.getElementById("navbar_rightlinks").style.fontSize = "25px";
     document.getElementById("nav_bar").style.opacity=".6";
   } else {
-    document.getElementById("nav_bar").style.padding = "80px 10px";
+    document.getElementById("nav_bar").style.padding = "100px 10px";
     document.getElementById("navbar_rightlinks").style.fontSize = "30px";
     document.getElementById("nav_bar").style.opacity=".9";
   }
