ba4df1b65ed9899ca9cde050712048f4d270fbf0
himynamesjordan <jlcates42@gmail.com>
2021-02-17 16:00:18 -0800

    tweak nav bar

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 window.onscroll = function() {scrollFunction()};
+onclick = alert("hello!");
 
 function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    document.getElementById("nav_bar").style.padding = "50px 10px";
-    document.getElementById("navbar_rightlinks").style.fontSize = "25px";
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("nav_bar").style.padding = "40px 10px";
+    document.getElementById("nav_bar").style.fontSize = "30px";
+    document.getElementById("navbar_rightlinks").style.fontSize = "15px";
     document.getElementById("nav_bar").style.opacity=".6";
-  } else {
-    document.getElementById("nav_bar").style.padding = "100px 10px";
-    document.getElementById("navbar_rightlinks").style.fontSize = "30px";
-    document.getElementById("nav_bar").style.opacity=".9";
+  }
+  else {
+    document.getElementById("nav_bar").style.padding = "50px 10px";
+    document.getElementById("nav_bar").style.fontSize = "40px";
+    document.getElementById("navbar_rightlinks").style.fontSize = "15px";
+    document.getElementById("nav_bar").style.opacity=".6";
   }
 } 
